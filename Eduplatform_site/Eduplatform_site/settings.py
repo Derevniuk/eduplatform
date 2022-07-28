d75f6772e0e83669c30e2bcfd9b96a9fe12f6323
shar <Sasha.sharai1998@mail.ru>
2022-07-29 00:49:16 +0300

    edit sittings.py

--- Eduplatform_site/Eduplatform_site/settings.py
+++ Eduplatform_site/Eduplatform_site/settings.py
@@ -16,31 +16,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ddnrxi2v!@f@54b2#7^!p-k%c3zx!*6u$xja^4s2_wkuhuox$z'
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-#
-# ALLOWED_HOSTS = []
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
-# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"),]
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,16 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Eduplatform_site.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
