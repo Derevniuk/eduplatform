1b92f836b8ef444781851619f5c959994d801d04
shar <Sasha.sharai1998@mail.ru>
2022-08-23 20:01:41 +0300

    add other models, add migrations

--- Eduplatform_site/learning/admin.py
+++ Eduplatform_site/learning/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Topic,Course
+from .models import (Topic, Course, Article, Test, Question, Answer, Attempt)
+
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('id', 'course_name')
@@ -11,5 +12,36 @@ class TopicAdmin(admin.ModelAdmin):
     list_display_link = ('id', 'topic_name', 'numbering')
 
 
-admin.site.register(Course,CourseAdmin)
-admin.site.register(Topic,TopicAdmin)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'topic')
+    list_display_link = ('id', 'title', 'topic')
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'topic')
+    list_display_link = ('id', 'title', 'topic')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+    list_display_link = ('id', 'text')
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+    list_display_link = ('id', 'text')
+
+
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'test', 'student')
+    list_display_link = ('id', 'test', 'student')
+
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Attempt, AttemptAdmin)
--- Eduplatform_site/learning/models.py
+++ Eduplatform_site/learning/models.py
@@ -1,34 +1,105 @@
-from django.utils.translation import gettext_lazy as _
-from django.core.validators import MinValueValidator
+
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.utils.translation import gettext_lazy as _
+
+from account.models import Teacher, Student
 from account.mixins import DateTimeMixinModel
-from account import models as account_model
-
-__all__ = {'Course', 'Topic'}
 
 
-class Course(models.Model,DateTimeMixinModel):
+__all__ = {'Course', 'Topic', 'Article', 'Test', 'Question', 'Answer', 'Attempt'}
+
+
+class Course(models.Model, DateTimeMixinModel):
     course_name = models.CharField(max_length=50)
-    creator = models.ForeignKey(account_model.Teacher,on_delete=models.CASCADE,null=True)
-
-    def __str__(self):
-        return f'{self.course_name}'
+    creator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("course")
         verbose_name_plural = _("courses")
 
+    def __str__(self):
+        return f'{self.course_name}'
 
-class Topic(models.Model,DateTimeMixinModel):
+
+class Topic(models.Model, DateTimeMixinModel):
     topic_name = models.CharField(max_length=50)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField(null=True)
-    numbering = models.IntegerField(validators=[MinValueValidator(0,'Min number value!')])
-
-    def __str__(self):
-        return f'{self.id}, {self.topic_name}, course - {self.course}'
+    numbering = models.IntegerField(validators=[MinValueValidator(0, 'Min number value!')])
 
     class Meta:
         verbose_name = _("course_topic")
         verbose_name_plural = _("course_topics")
-        ordering = ['numbering',]
+        ordering = ['numbering', ]
+
+    def __str__(self):
+        return f'{self.id}, {self.topic_name}, course - {self.course}'
+
+
+class Article(models.Model, DateTimeMixinModel):
+    creator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = _("topic_articles")
+        verbose_name_plural = _("topic_articles")
+
+
+class Test(models.Model, DateTimeMixinModel):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    creator = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    is_open = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = _("topic_test")
+        verbose_name_plural = _("topic_tests")
+
+
+class Question(models.Model, DateTimeMixinModel):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.text}"
+
+    class Meta:
+        verbose_name = _("test_question")
+        verbose_name_plural = _("test_questions")
+
+
+class Answer(models.Model, DateTimeMixinModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50, blank=True, null=True)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.text}"
+
+    class Meta:
+        verbose_name = _("question_answer")
+        verbose_name_plural = _("question_answers")
+
+
+class Attempt(models.Model, DateTimeMixinModel):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.test} - {self.student}"
+
+    class Meta:
+        verbose_name = _("test_attempt")
+        verbose_name_plural = _("test_attempts")
