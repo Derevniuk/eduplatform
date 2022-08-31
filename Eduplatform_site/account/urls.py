9ffd5b40355f7042a1480c14d31f6c87d9d0be2b
shar <Sasha.sharai1998@mail.ru>
2022-08-31 12:56:20 +0300

    changed APIView to ViewSet

--- Eduplatform_site/account/urls.py
+++ Eduplatform_site/account/urls.py
@@ -12,25 +12,4 @@ router.register(r'photos',PhotoViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
-    # path('teachers/',TeacherViewSet.as_view())
 ]
-# from .endpoints import (
-#     UserListAPIView,
-#     UserDetailApi,
-#     TeacherListAPIView,
-#     TeacherDetailApi,
-#     StudentListAPIView,
-#     StudentDetailApi,
-#     GroupListAPIView,
-#     GroupDetailApi)
-#
-# urlpatterns = [
-#     path('users/',UserListAPIView.as_view()),
-#     path('users/<int:pk>',UserDetailApi.as_view()),
-#     path('users/teachers/',TeacherListAPIView.as_view()),
-#     path('users/teachers/<int:pk>',TeacherDetailApi.as_view()),
-#     path('users/students/',StudentListAPIView.as_view()),
-#     path('users/students/<int:pk>',StudentDetailApi.as_view()),
-#     path('users/groups/',GroupListAPIView.as_view()),
-#     path('users/groups/<int:pk>',GroupDetailApi.as_view()),
-# ]
